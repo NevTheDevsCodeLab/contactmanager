a855fb19c5b64e0094ef413e8c0e74e4174a54ef
Neville Cox <neville.cox@outlook.com>
2019-01-17 11:50:34 +0000

    Updates

---
+++ precache-manifest.01b3fd2243050e033f9ffe2bded79554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "13a58ba6c6959b7d1021",
+    "url": "/contactmanager/static/js/main.13a58ba6.chunk.js"
+  },
+  {
+    "revision": "9271c52da71d3fbd6b7b",
+    "url": "/contactmanager/static/js/1.9271c52d.chunk.js"
+  },
+  {
+    "revision": "13a58ba6c6959b7d1021",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "9271c52da71d3fbd6b7b",
+    "url": "/contactmanager/static/css/1.11670a71.chunk.css"
+  },
+  {
+    "revision": "b1b7faeb1936b093ce7e1a2dcc33a171",
+    "url": "/contactmanager/index.html"
+  }
+];
